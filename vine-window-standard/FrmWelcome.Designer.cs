a04a4ae616be0279d36f8a1f520137af7caa95fc
张弓 <sz9214e@qq.com>
2017-09-27 22:39:47 +0800

    改进欢迎页显示尺寸

--- vine-window-standard/FrmWelcome.Designer.cs
+++ vine-window-standard/FrmWelcome.Designer.cs
@@ -40,7 +40,7 @@
             this.pictureBox1.Image = global::vine_window_standard.Properties.Resources.welcome;
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(784, 561);
+            this.pictureBox1.Size = new System.Drawing.Size(784, 421);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -56,7 +56,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.ClientSize = new System.Drawing.Size(784, 421);
             this.Controls.Add(this.pictureBox1);
             this.Name = "FrmWelcome";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
